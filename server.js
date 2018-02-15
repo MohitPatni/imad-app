ca806f8d698f774200bab408a07a58da3b8ea812
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-15 20:38:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one',function(req,res){
+    res.send("this is one");
+});
+
+app.get('/artical-two',function(req,res){
+    res.send("this is two");
+});
+
+app.get('/artical-three',function(req,res){
+    res.send("this is three");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
