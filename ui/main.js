173f149472eea755ff4c455571822f7654f8a365
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-16 23:37:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,16 @@ console.log('Loaded!');
 // change the text of main text div
 //var element = document.getElementById('main-text');
 //element.innerHTML = "Lets do something new";
-//animate image
+var button = document.getElementById('counter');
+var counter = 0;
+button.onClick = function(){
+  //make a response to the counter endpint
+  
+  
+  //capture the response and store into variable.
+  
+  //render the variable in the correct span.
+  counter = counter +1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+}; 
