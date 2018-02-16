9f3d28eb88bb3378d5a19d47dbbf7556bbbcd2b0
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-17 01:44:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,17 +25,17 @@ button.onclick = function(){
   request.send(null);
 }; 
 //submit name
-var nameinput = document.getElementById('name');
-var name = nameinput.value;
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make arequest to the server and send the name
     
     //capture the list of name and rander it
-    var name = ['name1','name2','name3','name4'];
+    var names = ['name1','name2','name3','name4'];
     var list ='';
     for (var i=0;i<name.length;i++){
-    list = '<li>'+ name +'</li>';
+    list = '<li>'+ names[i] +'</li>';
 }
 var ui =  document.getElementById('namelist');
 ui.innerHTML.list;
