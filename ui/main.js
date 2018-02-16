7e3c4152170d6fd99014b70565248f67332a854e
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-16 19:42:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,5 @@
 console.log('Loaded!');
 // change the text of main text div
-var element = document.getElementById('main-text');
-element.innerHTML = "Lets do something new";
+//var element = document.getElementById('main-text');
+//element.innerHTML = "Lets do something new";
 //animate image
-var main = function() {
-  $('#mj').click(function() {
-    $('#mj').animate({
-      left: '220px'
-    }, 200);
-
-  });
-};
