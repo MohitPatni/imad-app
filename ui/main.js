8cb68f202be9fb80c6cf90497918c72b17f002cb
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-17 01:37:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,19 @@ button.onclick = function(){
   request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/counter', true);
   request.send(null);
 }; 
+//submit name
+var nameinput = document.getElementById('name');
+var name = nameinput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //make arequest to the server and send the name
+    
+    //capture the list of name and rander it
+    var name = ['name1','name2','name3','name4'];
+    var list ='';
+    for (var i=0;i<name.length;i++){
+    list = '<li>'+ name +'</li>';
+}
+var ui =  document.getElementById('namelist');
+ui.innerHTML.list;
+        };
