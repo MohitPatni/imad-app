e5194d894b2f669a8d70da387a23828306b6182d
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-17 23:51:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,7 +53,7 @@ submit.onclick = function(){
 
   //make the request 
  var nameInput = document.getElementById('name');
- var name = nameInput.value;
+ var name = nameInput.Value;
   request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/submit-name?name='+ name, true);
   request.send(null);
 }; 
