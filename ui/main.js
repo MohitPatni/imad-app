d844bd64402b534eb0adaad5980f06612ca49e2f
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-17 11:33:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,18 +25,18 @@ button.onclick = function(){
   request.send(null);
 }; 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
-    //make arequest to the server and send the name
+// var nameInput = document.getElementById('name');
+// var name = nameInput.value;
+// var submit = document.getElementById('submit_btn');
+// submit.onclick = function(){
+//     //make arequest to the server and send the name
     
-    //capture the list of name and rander it
-    var names = ['name1','name2','name3','name4,name5'];
-    var list ='';
-    for (var i=0;i<names.length;i++){
-    list += '<li>'+ names[i] +'</li>';
-}
-var ul =  document.getElementById('namelist');
-ul.innerHTML = list;
-        };
+//     //capture the list of name and rander it
+//     var names = ['name1','name2','name3','name4,name5'];
+//     var list ='';
+//     for (var i=0;i<names.length;i++){
+//     list += '<li>'+ names[i] +'</li>';
+// }
+// var ul =  document.getElementById('namelist');
+// ul.innerHTML = list;
+//         };
