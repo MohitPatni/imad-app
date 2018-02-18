d1edd3d31e0746f52f42bf40b796b1c13782bb85
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-18 23:27:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,61 +2,7 @@
 // change the text of main text div
 //var element = document.getElementById('main-text');
 //element.innerHTML = "Lets do something new";
-         //comment
- var submit = document.getElementById('submit_id');
-    submit.onclick = function() {
-
-    //Create a request object
-   var request  = new XMLHttpRequest();
-   request.onreadystatechange = function () {
-    if(request.readyState === XMLHttpRequest.DONE) {
-       if(request.status === 200){
-         var comments = request.responseText;//request.responseText;
-             comments = JSON.parse(comments);  // convert a string back into an array.
-             var list ='';
-             for (var i=0 ; i < comments.length ; i++){
-                     list += '<li>'+ comments[i] +'</li>';
-                    }
-            var ul =  document.getElementById('commentlist');
-            ul.innerHTML = list; 
-       }
-  //takr a action
-     
-            }
-     };
-  //capture the response and store into variable.
- //Not done yet
- var commentInput = document.getElementById('commentid');
- var comment = commentInput.value;
-    request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/comment-one?comment='+comment, true);
-  request.send(null);
-};
-
- //counter  
-      var button = document.getElementById('counter');
-
-button.onclick = function(){
-  //Create a request object
-  var request  = new XMLHttpRequest();
-  
-  //capture the response and store into variable.
-  request.onreadystatechange = function () {
-    if(request.readyState === XMLHttpRequest.DONE) {
-        //take some action
-        if(request.status === 200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }
-   
-  };
-  //make the request 
-  request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/counter', true);
-  request.send(null);
-}; 
-
-//submit name
+         //submit name
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
@@ -88,6 +34,61 @@ submit.onclick = function() {
   request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/submit-name?name='+ name, true);
   request.send(null);
 }; 
+
+ //counter  
+      var button = document.getElementById('counter');
+
+button.onclick = function(){
+  //Create a request object
+  var request  = new XMLHttpRequest();
+  
+  //capture the response and store into variable.
+  request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        //take some action
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+   
+  };
+  //make the request 
+  request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/counter', true);
+  request.send(null);
+}; 
+
+
+         //comment
+ var submit = document.getElementById('submit_id');
+    submit.onclick = function() {
+
+    //Create a request object
+   var request  = new XMLHttpRequest();
+   request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE) {
+       if(request.status === 200){
+         var comments = request.responseText;//request.responseText;
+             comments = JSON.parse(comments);  // convert a string back into an array.
+             var list ='';
+             for (var i=0 ; i < comments.length ; i++){
+                     list += '<li>'+ comments[i] +'</li>';
+                    }
+            var ul =  document.getElementById('commentlist');
+            ul.innerHTML = list; 
+       }
+  //takr a action
+     
+            }
+     };
+  //capture the response and store into variable.
+ //Not done yet
+ var commentInput = document.getElementById('commentid');
+ var comment = commentInput.value;
+    request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/comment-one?comment='+comment, true);
+  request.send(null);
+};
 
  
  
