5069f9d04c2ac8eb32b651b96b529f3822db90f6
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-18 22:30:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,16 +139,16 @@ app.get('/:articleName',function(req,res){
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 var comments= [];
 app.get('/comment-one/:comment', function(req, res){
      var comment = req.params.comment;//extract name
   comments.push(comment);
   //JOSON javascrpit Object Notation
   res.send(JSON.stringify(comments));//json coverting aray into a string
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 
