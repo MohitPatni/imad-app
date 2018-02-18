5a26dd9126ef5ae72dee9d014d2572c087fcecb1
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-18 21:12:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,35 @@ button.onclick = function(){
   request.send(null);
 }; 
 
+ var submit = document.getElementById('submit_id');
+    submit.onclick = function() {
 
+    //Create a request object
+   var request  = new XMLHttpRequest();
+   request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE) {
+       if(request.status === 200){
+         var comments = request.responseText;//request.responseText;
+             comments = JSON.parse(comments);  // convert a string back into an array.
+             var list ='';
+             for (var i=0 ; i < comments.length ; i++){
+                     list += '<li>'+ comments[i] +'</li>';
+                    }
+            var ul =  document.getElementById('commentlist');
+            ul.innerHTML = list; 
+       }
+  //takr a action
+     
+            }
+     };
+  //capture the response and store into variable.
+ //Not done yet
+ var commentInput = document.getElementById('commentid');
+ var comment = commentInput.value;
+    request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/comment-one/'+comment, true);
+  request.send(null);
+};
+   
 
 
 
@@ -69,32 +97,4 @@ submit.onclick = function() {
  
      //comment
  
- var submit = document.getElementById('submit_id');
-    submit.onclick = function() {
 
-    //Create a request object
-   var request  = new XMLHttpRequest();
-   request.onreadystatechange = function () {
-    if(request.readyState === XMLHttpRequest.DONE) {
-       if(request.status === 200){
-         var comments = request.responseText;//request.responseText;
-             comments = JSON.parse(comments);  // convert a string back into an array.
-             var list ='';
-             for (var i=0 ; i < comments.length ; i++){
-                     list += '<li>'+ comments[i] +'</li>';
-                    }
-            var ul =  document.getElementById('commentlist');
-            ul.innerHTML = list; 
-       }
-  //takr a action
-     
-            }
-     };
-  //capture the response and store into variable.
- //Not done yet
- var commentInput = document.getElementById('commentid');
- var comment = commentInput.value;
-    request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/comment-one/'+comment, true);
-  request.send(null);
-};
-   
