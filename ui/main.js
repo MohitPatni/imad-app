9afa32b29301e42b9bf61985c16d94270c7629d4
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-18 20:11:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,30 +6,7 @@
 
 //capture the list of name and rander it
           //counter  
-var button = document.getElementById('counter');
-
-button.onclick = function(){
-  //Create a request object
-  var request  = new XMLHttpRequest();
-  
-  //capture the response and store into variable.
-  request.onreadystatechange = function () {
-    if(request.readyState === XMLHttpRequest.DONE) {
-        //take some action
-        if(request.status === 200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }
-   
-  };
-  //make the request 
-  request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/counter', true);
-  request.send(null);
-}; 
-
- //comment
+           //comment
  var commentInput = document.getElementById('commentid');
  var comment = commentInput.value;
  var submit = document.getElementById('submit_id');
@@ -58,6 +35,30 @@ button.onclick = function(){
     request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/comment-one', true);
   request.send(null);
 };
+var button = document.getElementById('counter');
+
+button.onclick = function(){
+  //Create a request object
+  var request  = new XMLHttpRequest();
+  
+  //capture the response and store into variable.
+  request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        //take some action
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+   
+  };
+  //make the request 
+  request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/counter', true);
+  request.send(null);
+}; 
+
+
 
 
 
