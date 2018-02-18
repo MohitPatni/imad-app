217252768e7485e48545527d59431843ec8a0618
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-18 16:53:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,7 +60,7 @@ submit.onclick = function() {
      var commentInput = document.getElementById('commentid');
  var comment = commentInput.value;
  var submit = document.getElementById('submit_id');
-    submit.onclick = function() {
+    button.onclick = function() {
 
     //Create a request object
 
