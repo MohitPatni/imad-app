711258ab24edd4770c664005fbb5d28346154a4b
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-18 22:37:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,32 @@
   request.send(null);
 };
 
-       
+    //counter  
+      var button = document.getElementById('counter');
+
+button.onclick = function(){
+  //Create a request object
+  var request  = new XMLHttpRequest();
+  
+  //capture the response and store into variable.
+  request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        //take some action
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+   
+  };
+  //make the request 
+  request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/counter', true);
+  request.send(null);
+}; 
+
+   
+          
 
 
 //submit name
@@ -67,31 +92,6 @@ submit.onclick = function() {
   request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/submit-name?name='+ name, true);
   request.send(null);
 }; 
-  //counter  
-      var button = document.getElementById('counter');
 
-button.onclick = function(){
-  //Create a request object
-  var request  = new XMLHttpRequest();
-  
-  //capture the response and store into variable.
-  request.onreadystatechange = function () {
-    if(request.readyState === XMLHttpRequest.DONE) {
-        //take some action
-        if(request.status === 200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }
-   
-  };
-  //make the request 
-  request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/counter', true);
-  request.send(null);
-}; 
-
-   
-     
  
 
