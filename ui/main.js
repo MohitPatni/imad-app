fdb4e78ed54fb182d8fa24ffd84ef9fab9fd5e34
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-18 17:46:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,34 @@
 // change the text of main text div
 //var element = document.getElementById('main-text');
 //element.innerHTML = "Lets do something new";
- var commentInput = document.getElementById('commentid');
+ 
+        
+    //capture the list of name and rander it
+            
+var button = document.getElementById('counter');
+
+button.onclick = function(){
+  //Create a request object
+  var request  = new XMLHttpRequest();
+  
+  //capture the response and store into variable.
+  request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        //take some action
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+   
+  };
+  //make the request 
+  request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/counter', true);
+  request.send(null);
+}; 
+//comment
+var commentInput = document.getElementById('commentid');
  var comment = commentInput.value;
  var submit = document.getElementById('submit_id');
     submit.onclick = function() {
@@ -30,31 +57,6 @@
     request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/comment', true);
   request.send(null);
 };
-        
-    //capture the list of name and rander it
-            
-var button = document.getElementById('counter');
-
-button.onclick = function(){
-  //Create a request object
-  var request  = new XMLHttpRequest();
-  
-  //capture the response and store into variable.
-  request.onreadystatechange = function () {
-    if(request.readyState === XMLHttpRequest.DONE) {
-        //take some action
-        if(request.status === 200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }
-   
-  };
-  //make the request 
-  request.open('GET', 'http://mohitpatni293.imad.hasura-app.io/counter', true);
-  request.send(null);
-}; 
 //submit name
 
 var submit = document.getElementById('submit_btn');
