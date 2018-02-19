20e0ff7518d8d8f75cd0dd74463f5e45a40e2bf0
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-20 02:15:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ app.get('/:articleName',function(req,res){
      res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/ui/profile.html', function (req, res) {
+app.get('/profile.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
 
