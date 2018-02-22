9631bd7588c22eb8c99b4792b34d1e20a6c1a592
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-22 12:03:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,6 +125,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/comment', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'commentme.html'));
+});
+
 var pool = new  pool(config);
 app.get('/test-db', function(req, res) {
     //make a select request
@@ -137,10 +142,6 @@ app.get('/test-db', function(req, res) {
       }
     });
 });
-app.get('/comment', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'commentme.html'));
-});
-
 app.get('/profile', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
