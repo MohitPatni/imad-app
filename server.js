ba078d229a6f98e5bd5e0ed2a930faccf6e95125
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-02-22 12:26:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     
     user: 'mohitpatni293',
     database: 'mohitpatni293',
-    host:'imad.hasura-app.io',
+    host:'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
 };
