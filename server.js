158ae0bd2d1af9de1a6ca9465aa32469f1aba530
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-03-01 09:17:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     
@@ -63,6 +64,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+function hash(input,salt){
+//how to create a hash
+var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+return hashed.toString('hex');
+    
+}
+app.get('/hash/:input', function (req, res){
+   var hashedString = hash(req.params.input,'this is some random string');
+   req.send(hashedString);
+});
 
 var pool = new Pool(config);
 app.get('/db-test', function (req, res) {
@@ -76,6 +87,8 @@ app.get('/db-test', function (req, res) {
      }
  });
 });
+
+
 
 app.get('/comment', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'commentme.html'));
