eb59f957867b383a72a8624a291e22a1bce2ed88
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-03-01 09:19:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ return hashed.toString('hex');
 }
 app.get('/hash/:input', function (req, res){
    var hashedString = hash(req.params.input,'this is some random string');
-   req.send(hashedString);
+   res.send(hashedString);
 });
 
 var pool = new Pool(config);
