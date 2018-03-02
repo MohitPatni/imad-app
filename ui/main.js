4bbc6ebbe7db7da4bb907f5a741f32a803ea5dad
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-03-02 13:11:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -69,7 +69,7 @@ submit.onclick = function() {
  console.log(username);
  console.log(password);
   request.open('POST', 'http://mohitpatni293.imad.hasura-app.io/login', true);
-  request.setRequestHeader('Content-type', 'application/json');
+  request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify( {username: username, password: password} ) );
 };
 
