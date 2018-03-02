c2440393ad83896aa537ef4494fa02a07eb9b270
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-03-02 21:27:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,6 +145,11 @@ app.get('/check-login', function (req, res){
     
 });
 
+app.get('/logout', function(req, res){
+   delete req.session.auth;
+   res.send('Logged out');
+});
+
 var pool = new Pool(config);
 app.get('/db-test', function (req, res) {
 //     //make a select request
