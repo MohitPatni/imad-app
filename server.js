0637f6652df5a6294e4a7893955f6b303e2e3681
MohitPatni <36480276+MohitPatni@users.noreply.github.com>
2018-03-24 15:51:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,33 +27,30 @@ function createTemplate (data){
    var date = data.date;
    var content = data.content;
 var htmlTemplate =`
-    <html>
-        <head>
-            <title>
-                ${title}
-            </title>
-             <meta name="viewport" content="width=device-width, initial-scale=1">
-              <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-        <body>
-            <div class="container">
-            <div class ="menu" >
-    
-                 <a href="/">Home</a>
-               
-                   </div>
-            <br/>
-            <hr/>
-            <h4>
-           ${heading}
-            </h4>
-            <div>
-               ${date.toDateString()}
-            </div>
-            <div>
+ <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date.toDateString()}
+              </div>
+              <div>
                 ${content}
-            </div>
-       <hr/>
+              </div>
+              <hr/>
               <h4>Comments</h4>
               <div id="comment_form">
               </div>
